ce3a0dfad10aa1f44363336fc0a64a78e3d41319
apc518 <andychamberlainmusic@gmail.com>
2021-11-13 16:26:37 -0800

    set up manifest.json

--- server/radroutes/urls.py
+++ server/radroutes/urls.py
@@ -19,5 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('manifest.json', TemplateView.as_view(template_name='manifest.json'))
 ]
