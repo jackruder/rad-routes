699e300cc6ed2d3f8123f2d914479a3f327251e5
jackruder <jackruder14@gmail.com>
2021-11-25 13:32:35 -0800

    added models to admin

--- backend/radroutes/admin.py
+++ backend/radroutes/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import (
+    User,
+    Area,
+    Face,
+    Book,
+    Climb,
+    BookReview,
+    UserLibrary,
+    UserPrivateAccess,
+)
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Area)
+admin.site.register(Face)
+admin.site.register(Book)
+admin.site.register(Climb)
+admin.site.register(BookReview)
+admin.site.register(UserLibrary)
+admin.site.register(UserPrivateAccess)
