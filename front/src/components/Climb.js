b975a9902ba53457c8dea7c696bd03f05ba0cb94
apc518 <andychamberlainmusic@gmail.com>
2021-12-01 17:24:24 -0800

    first full-stack functionality! hooray! the front end path /climb/<id> will display a card with that climbs information, pulled from the api. It will not work if the react app is on localhost in chrome, but it will work on firefox (because chrome disallows cors on localhost)

--- front/src/components/Climb.js
+++ front/src/components/Climb.js
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 
 const apiUrlBase = process.env.NODE_ENV === "production" ? "" : "http://localhost:8000/api";
 
-const getHeightString  = (height) => {
+const getHeightString = (height) => {
   const ft = `${Math.round(height * 3.28084)}ft`;
   const m = `${height}m`
   if(navigator.language.toLowerCase() === 'en-us'){
@@ -25,30 +25,10 @@ const types = {
   }
 }
 
-const  getCookie = (cName) => {
-  const name = cName + "=";
-  const cDecoded = decodeURIComponent(document.cookie); //to be careful
-  const cArr = cDecoded.split('; ');
-  let res;
-  cArr.forEach(val => {
-    if (val.indexOf(name) === 0)
-      res = val.substring(name.length);
-  })
-  return res;
-}
-
 export default function Climb() {
   const {id} = useParams();
 
-  const [climbObj, setClimbObj] = useState(null);
-
-  // // these defaults should be fetched from an api in reality
-  // // eslint-disable-next-line
-  // const [type, setType] = useState(types.ropedClimb);
-  // // eslint-disable-next-line
-  // const [grade, setGrade] = useState("5.11c / V7 / 5.10a");
-  // // eslint-disable-next-line
-  // const [height, setHeight] = useState(35);
+  const [climbObj, setClimbObj] = useState({climb_type: "", grade: "", height: 0});
 
   useEffect(() => {
     fetch(`${apiUrlBase}/climbs/${id}`)
@@ -61,37 +41,34 @@ export default function Climb() {
   }, []);
 
   return (
-    <>Data: {JSON.stringify(climbObj)}</>
-    // <Card
-    //   style={{ width: '36rem', margin: 10 }}
-    // >
-    //   <div style={{display: 'flex'}}>
-    //     <Card.Body>
-    //       <Card.Title>Cool Climb</Card.Title>
-    //       Type: <b>{type.name}</b> <br/>
-    //       Grade: <b>{grade}</b> <br/>
+    <Card
+      style={{ width: '36rem', margin: 10 }}
+    >
+      <div style={{display: 'flex'}}>
+        <Card.Body>
+          <Card.Title>{climbObj.climb_name}</Card.Title>
+          Type: <b>{climbObj.climb_type}</b> <br/>
+          Grade: <b>{climbObj.grade}</b> <br/>
 
-    //       {type === types.ropedClimb || true ? 
-    //         <div style={{flexGrow: 1}}>Height: <b>{getHeightString(height)}</b></div> :
-    //         <></>}
+          <div style={{flexGrow: 1}}>Height: <b>{getHeightString(climbObj.height)}</b></div>
 
-    //       <div style={{lineHeight: '3rem'}} aria-label="Links to the face and feature on which this climb resides.">
-    //         <Card.Link>Face</Card.Link>
-    //         <Card.Link>Feature</Card.Link><br/>
-    //       </div>
-    //       <Card.Title style={{ marginTop: '1rem' }}>Description</Card.Title>
-    //       <Card.Text>
-    //         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo ad saepe sit alias ab molestias consectetur esse magnam tempora neque.
-    //       </Card.Text>
-    //     </Card.Body>
-    //     <Card.Img variant="top" src="/static/assets/rock-climbing.jpeg" style={{ overflow: 'hidden', height: '100%' }} />
-    //   </div>
-    //   <Card.Body>
-    //       <Card.Title>Getting There</Card.Title>
-    //       <Card.Text>
-    //         Vitae alias aperiam. A autem temporibus veritatis minima dolore deserunt.
-    //       </Card.Text>
-    //   </Card.Body>
-    // </Card>
+          <div style={{lineHeight: '3rem'}} aria-label="Links to the face and feature on which this climb resides.">
+            <Card.Link>Face</Card.Link>
+            <Card.Link>Feature</Card.Link><br/>
+          </div>
+          <Card.Title style={{ marginTop: '1rem' }}>Description</Card.Title>
+          <Card.Text>
+            {climbObj.description}
+          </Card.Text>
+        </Card.Body>
+        <Card.Img variant="top" src="/static/assets/rock-climbing.jpeg" style={{ overflow: 'hidden', height: '100%' }} />
+      </div>
+      <Card.Body>
+          <Card.Title>Getting There</Card.Title>
+          <Card.Text>
+            Vitae alias aperiam. A autem temporibus veritatis minima dolore deserunt.
+          </Card.Text>
+      </Card.Body>
+    </Card>
   )
 }
