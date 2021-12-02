dbbeb1ed990625ce16097b14c96a250e22f7a116
apc518 <andychamberlainmusic@gmail.com>
2021-12-01 17:07:48 -0800

    got CORS working on FIREFOX ONLY chrome does not allow cors on localhost for some reason... firefox does so firefox will have to be used to develop the front-end. Fortunately when the front end is built and deployed it will be same-origin so cors wont matter

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,14 +46,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -132,8 +132,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "front-build/static")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-    'http://localhost:3000'
-)
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
