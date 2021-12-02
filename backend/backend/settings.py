54b0c2ddeecbd12967e3477fe57a81e56d008549
jackruder <jackruder14@gmail.com>
2021-12-01 16:07:06 -0800

    fixed cors issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "radroutes.apps.RadroutesConfig",
-#    "corsheaders",
+    "corsheaders",
     "rest_framework",
 ]
 
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-#    "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -132,4 +132,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "front-build/static")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ORIGIN_ALLOW_ALL = True
