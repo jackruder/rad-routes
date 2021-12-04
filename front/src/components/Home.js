a27233e297ac2115598004a875c0e9c2238cf306
apc518 <andychamberlainmusic@gmail.com>
2021-12-04 01:05:51 -0800

    home page with hero

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-export default function Home(){
+export default function Home() {
     return (
-        <>Imagine Some Rad Routes</>
+        <div style={{
+            display: 'flex',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundImage: "url(/static/assets/banner-1.jpg)",
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center'
+        }}>
+            <div style={{ display: 'flex' }}>
+                <h1 style={{
+                    fontSize: 72,
+                    animation: "splash 1s normal forwards ease-in-out"
+                }}>
+                    "Sick Route, Dude"
+                </h1>
+                <div style={{ minWidth: '33vw' }}/>
+            </div>
+        </div>
     )
 }
