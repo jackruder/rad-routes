eb6e9f7e09fdca91851fbe0e50cbe05ac952cd4a
apc518 <andychamberlainmusic@gmail.com>
2021-12-04 01:05:19 -0800

    added hero homepage

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
@@ -6,19 +5,24 @@ import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Search from './components/Search';
 import Climb from './components/Climb';
+import ClimbList from './components/ClimbList';
 import EditPortal from './components/EditPortal';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/climb/:id" element={<Climb/>}/>
-        <Route exact path="/signup" element={<SignUp/>}/>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/search" element={<Search/>}/>
-        <Route exact path="/editportal" element={<EditPortal/>}/>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/climbs" element={<ClimbList/>}/>
+          <Route exact path="/climbs/:id" element={<Climb/>}/>
+          <Route exact path="/signup" element={<SignUp/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/search" element={<Search/>}/>
+          <Route exact path="/editportal" element={<EditPortal/>}/>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
