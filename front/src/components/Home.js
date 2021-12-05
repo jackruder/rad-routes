f8e7b9f6e6265042fd4f93444c5e83c18e1f2d40
apc518 <andychamberlainmusic@gmail.com>
2021-12-05 15:34:45 -0800

    restyled homepage a bit w/Dom - added some fun buttons we might implement in the future: random climb and climb of the day - changed navbar to have less padding and be dark theme

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const buttons = [
+    {
+        text: "Climb of the Day"
+    },
+    {
+        text: "Random Climb"
+    }
+]
+
 export default function Home() {
     return (
         <div style={{
@@ -13,12 +26,30 @@ export default function Home() {
             backgroundPosition: 'center'
         }}>
             <div style={{ display: 'flex' }}>
-                <h1 style={{
-                    fontSize: 72,
-                    animation: "splash 1s normal forwards ease-in-out"
+                <div id="title" style={{
+                    textAlign: 'center'
                 }}>
-                    "Sick Route, Dude"
-                </h1>
+                    <h1 style={{
+                        fontSize: 72,
+                        animation: "splash 1s normal forwards ease-in-out",
+                    }}>
+                        "Rad Route, Dude"
+                    </h1>
+                    <br/>
+                    <Row xs={1} md={2} style={{ width: '65%', margin: 'auto' }}>
+                        {buttons.map((btn, idx) => (
+                            <Col 
+                                key={idx}
+                                style={{
+                                    marginTop: 10
+                                }}>
+                                <Button variant="dark">{btn.text}</Button>
+                            </Col>
+                            // TODO add vertical padding here
+                        ))}
+                    </Row>
+                    
+                </div>
                 <div style={{ minWidth: '33vw' }}/>
             </div>
         </div>
